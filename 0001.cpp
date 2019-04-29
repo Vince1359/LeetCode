a3f246db5d31f658c1f33c9b520650ec1c4ad11e
Vince1359 <kt1359@vip.qq.com>
2019-04-29 16:15:48 +0800

    更新第一题和第二题的解答

--- 0001.cpp
+++ 0001.cpp
@@ -1,23 +1,31 @@
+/*
+ * 1. Two Sum
+ * Given an array of integers, return indices of the two numbers such that they add up to a 
+ * specific target. You may assume that each input would have exactly one solution, 
+ * and you may not use the same element twice.
+ * 
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
 #include <vector>
 #include <unordered_map>
 #include <stdexcept>
 
-using namespace std;
-
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		unordered_map<int, int> record;
-		for (int i = 0; i < nums.size(); i++) {
-
-			int complement = target - nums[i];
+	static std::vector<int> two_sum(std::vector<int>& nums, const int target) {
+		std::unordered_map<int, int> record;
+		for (auto i = 0; i < nums.size(); i++) {
+			auto complement = target - nums[i];
 			if (record.find(complement) != record.end()) {
 				int res[] = { i, record[complement] };
-				return vector<int>(res, res + 2);
+				return std::vector<int>(res, res + 2);
 			}
-
 			record[nums[i]] = i;
 		}
-		throw invalid_argument("the input has no solution");
+		throw std::invalid_argument("the input has no solution");
 	}
 };
--- 0002.cpp
+++ 0002.cpp
@@ -1,21 +1,34 @@
+/*
+ * 2. Add Two Numbers
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit. 
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * 
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+
 struct ListNode {
 	int val;
 	ListNode *next;
-	ListNode(int x) : val(x), next(nullptr) {}
+	explicit ListNode(const int x) : val(x), next(nullptr) {}
 };
  
 class Solution {
 public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		int len1 = getLen(l1), len2 = getLen(l2);
-		ListNode* p1 = len1 > len2 ? l1 : l2;
-		ListNode* p2 = len1 > len2 ? l2 : l1;
+	static ListNode* add_two_numbers(ListNode* l1, ListNode* l2) {
+		const auto len1 = get_len(l1), len2 = get_len(l2);
+		auto p1 = len1 > len2 ? l1 : l2;
+		auto p2 = len1 > len2 ? l2 : l1;
 
 		ListNode* pre = nullptr;
-		int carried = 0;
+		auto carried = 0;
 		while (p1) {
-			int a = p1->val;
-			int b = p2 ? p2->val : 0;
+			const auto a = p1->val;
+			const auto b = p2 ? p2->val : 0;
 			p1->val = (a + b + carried) % 10;
 			carried = (a + b + carried) / 10;
 
@@ -23,13 +36,13 @@ public:
 			p1 = p1->next;
 			p2 = p2 ? p2->next : nullptr;
 		}
-
 		pre->next = carried ? new ListNode(1) : nullptr;
+
 		return len1 > len2 ? l1 : l2;
 	}
 private:
-	int getLen(ListNode * l) {
-		int res = 0;
+	static int get_len(ListNode * l) {
+		auto res = 0;
 		while (l)
 			res++, l = l->next;
 		return res;
