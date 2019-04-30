cb6d2feb3a6af9b28146563b33a9bbebf244495d
Vince1359 <kt1359@vip.qq.com>
2019-04-30 10:48:30 +0800

    提交第66题解答

--- 0066.cpp
+++ 0066.cpp
@@ -1,20 +1,37 @@
+/*
+ * 66. Plus One
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * The digits are stored such that the most significant digit is at the head of the list, 
+ * and each element in the array contain a single digit.
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ * 
+ * Example 1:
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * 
+ * Example 2:
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ */
+
 #include <vector>
 
 class Solution {
 public:
-	std::vector<int> plusOne(std::vector<int>& digits) {
+	static std::vector<int> plus_one(std::vector<int>& digits) {
 		digits[digits.size() - 1] ++;
-		for (int i = digits.size() - 1; i >= 1; i--)
+		for (int i = digits.size() - 1; i >= 1; i--) {
 			if (digits[i] == 10) {
 				digits[i] = 0;
 				digits[i - 1] ++;
 			}
-
+		}
 		if (digits[0] == 10) {
 			digits[0] = 0;
 			digits.insert(digits.begin(), 1);
 		}
-
 		return digits;
 	}
 };
