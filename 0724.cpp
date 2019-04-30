02d4b917e85c4547ecdd3f83586f588855141725
Vince1359 <kt1359@vip.qq.com>
2019-04-30 11:19:03 +0800

    提交第724题解答

--- 0724.cpp
+++ 0724.cpp
@@ -1,21 +1,46 @@
+/*
+ * 724. Find Pivot Index
+ * Given an array of integers nums, write a method that returns the "pivot" index of this array.
+ * We define the pivot index as the index where the sum of the numbers to the left of the index
+ * is equal to the sum of the numbers to the right of the index.
+ * If no such index exists, we should return -1. If there are multiple pivot indexes, 
+ * you should return the left-most pivot index.
+ * 
+ * Example 1:
+ * Input:
+ * nums = [1, 7, 3, 6, 5, 6]
+ * Output: 3
+ * Explanation: 
+ * The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers
+ * to the right of index 3. Also, 3 is the first index where this occurs.
+ *  
+ * Example 2:
+ * Input: 
+ * nums = [1, 2, 3]
+ * Output: -1
+ * Explanation: 
+ * There is no index that satisfies the conditions in the problem statement.
+ */
+
 #include <numeric>
 #include <vector>
 
-class Solution {
+class Solution0724 {
 public:
-	int pivotIndex(std::vector<int>& nums) {
-		if (nums.size() == 0)
+	int pivot_index(std::vector<int>& nums) const
+	{
+		if (nums.empty())
 			return -1;
 
-		int sum = std::accumulate(nums.begin(), nums.end(), 0);
+		const auto sum = std::accumulate(nums.begin(), nums.end(), 0);
 
-		int leftSum = 0;
-		for (int i = 0; i < nums.size(); i++)
-			if (leftSum == sum - leftSum - nums[i])
+		auto left_sum = 0;
+		for (auto i = 0; i < nums.size(); i++)
+		{
+			if (left_sum == sum - left_sum - nums[i])
 				return i;
-			else
-				leftSum += nums[i];
-
+			left_sum += nums[i];
+		}
 		return -1;
 	}
 };
